c5cdd8b0d23f97cdfca4007114f4d7aac4acfc98
kulmiye73 <kulmiye73@student.rtc.edu>
2020-12-01 15:25:52 -0800

    good ping

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-# This is the template code for the CNA337 Final Project
-# Zachary Rubin, zrubin@rtc.edu
-# CNA 337 Fall 2020
+#abdirizak kulmiyee
+#CNA337 Fall 2020
 
+
+import os
 def print_program_info():
     # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
-
+    print("Server Automator v0.1 by abdirizak")
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
-    # TODO - Create a Server object
-    # TODO - Call Ping method and print the results
+
+
+public_ec2= "3.128.180.44"
+responce = os.system('ping -n 4 ' + public_ec2)
+# TODO - Create a Server object
+# TODO - Call Ping method and print the results
+if responce == 0:
+    print()
+    print(public_ec2, 'is up!')
+else:
+    print(public_ec2, 'is down!')
