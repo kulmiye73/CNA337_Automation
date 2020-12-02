32d794a3c2567dc289f141b7ce0c1adf645cb498
kulmiye73 <kulmiye73@student.rtc.edu>
2020-12-01 21:01:25 -0800

    ping

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
-#abdirizak kulmiyee
-#CNA337 Fall 2020
+# Pinging AWS EC2 public IPv4.
+
+# I worked with # Pinging AWS EC2 public IPv4.
+# # I worked with Vlado, Dorin and Igor, Mohammad and Seid on this code.
+#
+
+# Liviu Patrisco (Liviu_patrisco@hotmail.com) helped us to write the cod
+
+# CNA 337 Fall 2020
+
+from Server import Server
 
 
-import os
 def print_program_info():
     # TODO - Change your name
-    print("Server Automator v0.1 by abdirizak")
+    print("Server Automator v0.1 by yours")
+
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
-
-
-public_ec2= "3.128.180.44"
-responce = os.system('ping -n 4 ' + public_ec2)
-# TODO - Create a Server object
-# TODO - Call Ping method and print the results
-if responce == 0:
-    print()
-    print(public_ec2, 'is up!')
-else:
-    print(public_ec2, 'is down!')
+    ec2_ip_address = "3.128.180.44"
+    server = Server(ec2_ip_address)
+    result = server.ping()
+    print(result)
+    if result == 0:
+        print("Server with ip [%s] is up." % ec2_ip_address)
+    else:
+        print("Server with ip [%s] is down." % ec2_ip_address)
